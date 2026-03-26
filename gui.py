066b9dd3411c4c3e1c6a77739e4def5441d1a0c0
mephistr <nemcheen@gmail.com>
2026-03-26 18:25:16 +0500

    add files

---
+++ gui.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from PIL import Image, ImageTk, Image as PILImage
+from main import bot_turn, player_turn, move_result, main_game, possible_move, game_score
+
+
+def create_move_label(root, img_path, x, y, name):
+    """Создаёт лейбл с картинкой и биндит клик"""
+    img = PILImage.open(img_path)
+    scaled = img.resize((150, 150), PILImage.Resampling.LANCZOS)
+    photo = ImageTk.PhotoImage(scaled)
+    
+    label = tk.Label(root, image=photo, bg="gray")
+    label.name = name
+    label.image = photo  # не дать GC удалить
+    label.place(x=x, y=y)
+    
+    label.bind("<Button-1>", on_click)  # клик для всех
+    label.configure(cursor="hand2")
+    return label
+
+
+def on_click(event):
+    name = event.widget.name
+    move_result(name, bot_turn())
+
+
+root = tk.Tk()
+root.configure(bg="gray")
+root.geometry("800x600")
+
+create_move_label(root, "scissors.png", 600, 50, "scissors")
+create_move_label(root, "stone.png", 400, 80, "stone")
+create_move_label(root, "paper.png", 500, 280, "paper")
+
+
+root.mainloop()
--- main.py
+++ main.py
@@ -2,46 +2,52 @@ from typing import List
 import random as rd
 
 
-possible_move = ['stone', 'scissors', 'paper']
+possible_move = ['stone', 'paper', 'scissors']
 game_score = {'total_move': 0,
               'player_score': 0,
               'bot_score': 0}
 
-def bot_turn(possible_move: List[str]) -> str:
+def bot_turn() -> str:
     bot_move = rd.choice(possible_move)
+    print(f'Bot move is {bot_move}.')
     return bot_move
 
 def player_turn() -> str:
-    player_move = input()
+    player_input = input("Ваш ход: Камень/Ножницы/Бумага (К/Н/Б).\n \"З\" - Остановить игру\n")
+    if player_input.upper() == "К":
+        player_move = possible_move[0]
+    elif player_input.upper() == "Н":
+        player_move = possible_move[2]
+    elif player_input.upper() == "Б":
+        player_move = possible_move[1]
+    elif player_input.upper() == "З":
+        player_move = 'З'
+    else:
+        print('Введи корректный ход')
+        return player_turn() 
+          
+    print(f'Your move is {player_move}.')
     return player_move
 
 def move_result(player_move: str, bot_move: str) -> None:
-    if player_move.upper() == "К":
-        enter = possible_move[0]
-    elif player_move.upper() == "Н":
-        enter = possible_move[1]
-    elif player_move.upper() == "З":
-        exit()
-    else:
-        enter = possible_move[2]
-    print(f"Your move: {enter}\n Bot move: {bot_move}")
     game_score['total_move'] += 1
-    if (possible_move.index(enter) - 1 == possible_move.index(bot_move)) or (possible_move.index(enter) == 0 and possible_move.index(bot_move) == 2):
-        game_score['bot_score'] += 1
-        print('bot win')
-    elif possible_move.index(enter) == possible_move.index(bot_move):
-        print('nothing win')
-    else:
+    idx_player_move = possible_move.index(player_move)
+    idx_bot_move = possible_move.index(bot_move)
+    if (idx_player_move - idx_bot_move) == 1 or (idx_player_move - idx_bot_move) == -2:
         game_score['player_score'] += 1
-        print('player win')
+        print('Player win')
+    elif player_move == bot_move:
+        print('draw')
+    else:
+        game_score['bot_score'] += 1
+        print('Bot win')
 
 
 def main_game() -> None:
-    pass
+    while (player_move := player_turn()) != 'З':
+        bot_move = bot_turn()
+        move_result(player_move, bot_move)
 
-print('Если хотите завершить игру и увидеть счёт, введите З')
-vvod = input()
-while vvod != 'З':
-    move_result(vvod, bot_turn(possible_move))
-    vvod = input()
-print(f"Всего игр было сыграно {game_score['total_move']}.\nочки игрока равны {game_score['player_score']}.\nочки бота равны {game_score['bot_score']}.")
+if __name__ == '__main__':
+    main_game()
+    print(f"Всего игр было сыграно {game_score['total_move']}.\nочки игрока равны {game_score['player_score']}.\nочки бота равны {game_score['bot_score']}.")
